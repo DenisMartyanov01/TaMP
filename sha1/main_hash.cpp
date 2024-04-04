2b8bf762b00e33a73754eb523012cad8c27f50c1
bo! <82774847+DenisMartyanov01@users.noreply.github.com>
2024-04-04 17:38:43 +0300

    hash

---
+++ sha1/main_hash.cpp
@@ -0,0 +1,20 @@
+﻿#include "sha1.hpp"
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+int main(int argc, const char* argv)
+{
+    string input = "sdtgsdfd";              // Ввод строки
+
+    SHA1 checksum;
+    checksum.update(input);
+    const string hash = checksum.final();   // Вывод хеш - строки
+
+    cout << input << '\n' << hash << endl;  // Печать
+
+    return 0;
+}
